506617225df558d9a628a55a6abab41a52ac61df
Bᴀᴅsʜᴀ Sᴛᴜᴅɪᴏs <141768309+AkdBadsha@users.noreply.github.com>
2023-08-30 20:31:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Linkbot:pass@cluster0.0caigo2.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "webs")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Custom Chats
